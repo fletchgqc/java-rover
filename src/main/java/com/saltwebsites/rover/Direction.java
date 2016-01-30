d60ba31ece48133d4c68bbf73bea3533df823819
John Fletcher <jnfe@gft.com>
2016-01-30 23:08:30 +0100

    added logic to direction class

--- src/main/java/com/saltwebsites/rover/Direction.java
+++ src/main/java/com/saltwebsites/rover/Direction.java
@@ -2,6 +2,38 @@ package com.saltwebsites.rover;
 
 public enum Direction {
 
-    N, S, E, W;
+    N(0, 0, 1), E(1, 1, 0), S(2, 0, -1), W(3, -1, 0);
 
+    private int order;
+
+    private int xIncreaseOfForwardMovement;
+
+    private int yIncreaseOfForwardMovement;
+
+    private Direction(int order, int xIncreaseOfForwardMovement, int yIncreaseOfForwardMovement) {
+        this.order = order;
+        this.xIncreaseOfForwardMovement = xIncreaseOfForwardMovement;
+        this.yIncreaseOfForwardMovement = yIncreaseOfForwardMovement;
+    }
+
+    public Direction fromOrder(int order) {
+        for (Direction direction : Direction.values()) {
+            if (direction.getOrder() == order) {
+                return direction;
+            }
+        }
+        throw new IllegalArgumentException(String.valueOf(order) + " is not a valid value of order");
+    }
+
+    public int getOrder() {
+        return order;
+    }
+
+    public int getxIncreaseOfForwardMovement() {
+        return xIncreaseOfForwardMovement;
+    }
+
+    public int getyIncreaseOfForwardMovement() {
+        return yIncreaseOfForwardMovement;
+    }
 }
